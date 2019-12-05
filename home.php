d3b53f27b3674f7aec4e75299a6613fde0000a5b
rozhkovArtem <56875168+rozhkovArtem@users.noreply.github.com>
2019-12-06 01:37:01 +0500

    Create home.php

---
+++ home.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Kaushan+Script|Montserrat:400,700&display=swap&subset=cyrillic-ext" rel="stylesheet">
+  <title>IU4-12B</title>
+</head>
+
+<body>
+
+  <header class="header">
+    <div class="container">
+      <div class="header_inner">
+        <div class="header_logo">IU4-12B</div>
+
+        <nav class="nav">
+          <a class="nav_link active" href="#">About</a>
+          <a class="nav_link" href="#">Servise</a>
+          <a class="nav_link" href="#">Blog</a>
+          <a class="nav_link" href="#">Work</a>
+          <a class="nav_link" href="#">Contact</a>
+        </nav>
+
+      </div>
+    </div>
+  </header>
+
+  <div class="intro">
+    <div class="container">
+      <div class="intro_inner">
+        <h2 class="intro_suptitle">Creative template</h2>
+        <h1 class="intro_title">Welcome to IU4-12B</h1>
+
+        <a class="btn" href="#">Learn More</a>
+      </div>
+    </div>
+  </div>
+  <section class="section">
+    <div class="container">
+      <div class="section_header">
+        <h3 class="section_suptitle">What we do</h3>
+        <h2 class="section_title">Story about us</h2>
+        <div class="section_text">
+          <p>Здесь будет какой-то текст Здесь будет какой-то текст Здесь будет какой-то текст Здесь будет какой-то текст Здесь будет какой-то текст </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
+
+</html>
