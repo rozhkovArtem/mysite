537dbdb7cecee87f177e092d6fa9aa839fc5486d
rozhkovArtem <56875168+rozhkovArtem@users.noreply.github.com>
2019-12-10 11:29:49 +0500

    Create catalog.php

---
+++ catalog.php
@@ -0,0 +1,44 @@
+<?php
+include("auth.php");
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Каталог</title>
+    <link rel="stylesheet" href="assets/css/style.css" />
+</head>
+
+<body>
+  <?php
+include("auth.php");
+?>
+<header class="header">
+    <div class="container">
+      <div class="header_inner">
+        <div class="header_logo">IU4-12B </div>
+
+        <nav class="nav">
+          <a class="nav_link" href="home.php">Домашняя</a>
+          <a class="nav_link active" href="#">Каталог</a>
+          <a class="nav_link" href="#">Отзывы и предложения</a>
+          <a class="nav_link" href="logout.php">Выход</a>
+        </nav>
+
+      </div>
+    </div>
+  </header>
+
+  <div class="intro">
+    <div class="container">
+      <div class="intro_inner">
+        <h2 class="intro_suptitle">Каталог</h2>
+      
+      </div>
+    </div>
+  </div>
+
+</body>
+
+</html>
